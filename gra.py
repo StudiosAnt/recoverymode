b967d4b9cdc81be0e53193a8bb04d5b2116bfb68
StudiosAnt <feliksgwardzik@icloud.com>
2026-02-19 17:49:51 +0100

    Update gra.py

--- gra.py
+++ gra.py
@@ -387,7 +387,7 @@ def game_run():
         gracz.sety(gracz.ycor() + gracz.dy)
     
         # kolizja z platformą (tylko kwadraty!)
-        for k in kwadrat:
+        for k in kwadraty:
             if abs(k.xcor() - gracz.xcor()) < kwadrat:
                 if gracz.dy <= 0:  # spada w dół
                 # jeśli gracz wleciał w platformę lub jest na niej
